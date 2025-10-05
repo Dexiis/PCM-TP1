e6a08abe5f75bfbeaa7a969e479cd6bf77cb1c84
David Santos <daapsyt@gmail.com>
2025-10-05 01:21:33 +0100

    Sections update

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbarToggler = document.querySelector(".navbar-toggler");
-  if (navbarToggler) {
-    navbarToggler.addEventListener("click", function () {
-      navbarToggler.classList.toggle("change");
-    });
-  }
-});
